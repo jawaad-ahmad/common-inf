2b91bee0781b7e905263d49f82bccb07eb46824c
Jawaad Ahmad <jawaad.ahmad@gmail.com>
2018-05-07 22:50:37 -0500

    modify prep script to use zero script

--- scripts/export-prep.sh
+++ scripts/export-prep.sh
@@ -4,6 +4,12 @@
 #
 # TODO Mention that this script is used to wipe a VM before it is managed by Ansible, so it needs to be available as a single file with no dependencies for ease of use by downloading via a single wget command.
 
+DIRNAME="/usr/bin/dirname"
+
+PROGPATH="$(${DIRNAME} ${0})"
+
+ZERO_FREE_SPACE="${PROGPATH}/zero-free-space.sh"
+
 # TODO Mention references:
 # References:
 #    https://lonesysadmin.net/2013/03/26/preparing-linux-template-vms/
@@ -146,69 +152,8 @@
    # Step 11
    #---
 
-   echo "TODO --- we are here ---"
+   ${ZERO_FREE_SPACE}
 
-#   # Zero out all free space - zero out every single filesystem plus the extra unused VG space, so you can better compress the image. We have a script that goes into every filesystem, dd's a bunch of zeroes until it fills up, then removes the file. Same for a VG with empty space, create an LV that fills 100%FREE, zero it out and remove it.
-#   # Note this effectively un-thins the disks, so make sure enough free space is available before running.
-#
-#   # This script fills up 98% of each filesystem; for a template we can up this to 100%.
-#cat << END_OF_FILE > /dev/null
-##!/bin/sh
-#
-## TODO update recommendations:
-##    o  there are a few basic errors in the disk zeroing script.
-##       o  you want $4 (Available) not $3 (Used) for determining how much to zero.
-##          o  Negative on the $4 versus $3 — the way the df output happens the column says “Used” but it’s the available blocks as a number.
-##       o  PREFIX is not set.
-##          o  I did correct the PREFIX problem, my cut & paste omitted the block that figured out which RHEL version we’re running on and adapted.
-##       o  the case where there is no free space in LVM just generates an error.
-##    o  change to use mktemp instead of always using the file "zf"; just to
-##       make sure you don't overwrite any files that might ahve been created
-##       called zf
-##       o  Interestingly enough it used to use mktemp but with a static file name it was easier to have our monitoring system detect the file and not alarm on the transient disk full condition, until that file was more than 30 minutes since last access. A better way might be to write the name of the file to a temp file it tests for, but it was easy enough to do it this other way.
-##    o  you shouldn't need to call sync or sleep before removing the LV (although it doesn’t hurt). This is because sync flushes the file system buffers, but because you’re writing directly to a block device and not the file system, it doesn’t do anything. But you probably should add a sync after you do the DD to the file system, as it’s possible (although unlikely with a sufficiently large file) that the output to the ‘zf’ file won’t be make it to the block device! Alternatively, you can use the dd “conf=fdatasync” argument to imply this.
-##    o  set the DD block size explicitly – the default is 512 bytes – and this may take a long time to write if you have a large block device or file system.
-#
-###!/bin/sh
-##
-### Determine the version of RHEL
-##COND=`grep -i Taroon /etc/redhat-release`
-##if [ "$COND" = "" ]; then
-##        export PREFIX="/usr/sbin"
-##else
-##        export PREFIX="/sbin"
-##fi
-##
-##FileSystem=`grep ext /etc/mtab| awk -F" " '{ print $2 }'`
-##
-##for i in $FileSystem
-##do
-##        echo $i
-##        number=`df -B 512 $i | awk -F" " '{print $3}' | grep -v Used`
-##        echo $number
-##        percent=$(echo "scale=0; $number * 98 / 100" | bc )
-##        echo $percent
-##        dd count=`echo $percent` if=/dev/zero of=`echo $i`/zf
-##        /bin/sync
-##        sleep 15
-##        rm -f $i/zf
-##done
-##
-##VolumeGroup=`$PREFIX/vgdisplay | grep Name | awk -F" " '{ print $3 }'`
-##
-##for j in $VolumeGroup
-##do
-##        echo $j
-##        $PREFIX/lvcreate -l `$PREFIX/vgdisplay $j | grep Free | awk -F" " '{ print $5 }'` -n zero $j
-##        if [ -a /dev/$j/zero ]; then
-##                cat /dev/zero > /dev/$j/zero
-##                /bin/sync
-##                sleep 15
-##                $PREFIX/lvremove -f /dev/$j/zero
-##        fi
-##done
-##END_OF_FILE
-#
 #   # After finishing these last steps, use a trick depending on the kind of image being created:
 #   #    For VMware, use vMotion to re-thin the VM; or clone the image into a new VM, with the destination set as thin provisioning.. When doing this, all the contiguous zeros will be converted into nothing.
 #   #    For openstack/kvm images, you might want to convert them using qemu-img to qcow2 (if your provider supports it), and enable the compress flag: qemu-img convert -f raw -O qcow2 -c source.raw destination.qcow2
